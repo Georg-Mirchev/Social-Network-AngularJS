90b9586573c06a7f673030dfec9308236b1d09a7
Joro <infreezer@gmail.com>
2015-05-22 23:15:01 +0300

    edit cover image added and validation for images

--- app/js/controllers/EditProfileController.js
+++ app/js/controllers/EditProfileController.js
@@ -15,20 +15,32 @@ socialNetworkApp.controller('EditProfileController',
 
         $scope.getDataAboutMe();
 
-        $scope.uploadImage = function(fileInputField) {
+        $scope.uploadImage = function (fileInputField, isCoverImage) {
+            var sizeLimit;
+            isCoverImage ? sizeLimit = 1024000 : sizeLimit = 128000;
 
             var file = fileInputField.files[0];
-            if (file.type.match(/image\/.*/)) {
+            if (file && file.type.match(/image\/.*/) && file.size < sizeLimit) {
+                console.log('im in');
                 var reader = new FileReader();
-                reader.onload = function() {
-                    $('#profileImagePreview').attr('src', reader.result);
-                    $scope.userData.profileImageData = reader.result;
+                reader.onload = function () {
+                    if (isCoverImage) {
+                        $('#coverImagePreview').attr('src', reader.result);
+                        $scope.userData.coverImageData = reader.result;
+                    } else {
+                        $('#profileImagePreview').attr('src', reader.result);
+                        $scope.userData.profileImageData = reader.result;
+                    }
                 };
                 reader.readAsDataURL(file);
-            } else {
-                Notification.info('File not supported!');
-                $('#profileImagePreview').attr('src', '');
-                $scope.userData.profileImageData = null;
+            } else if (file) {
+                if (isCoverImage) {
+                    file.size > sizeLimit ? Notification.info('Cover image size limit is 1024kb.') :
+                        Notification.info('File not supported!');
+                } else {
+                    file.size > sizeLimit ? Notification.info('Profile picture size limit is 128kb.') :
+                        Notification.info('File not supported!');
+                }
             }
         };
 
